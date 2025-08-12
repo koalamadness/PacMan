5e0653457dac0d08f3a05f6ccc34989b4e6b60f4
YeQiu <YeQiu@example.com>
2025-08-12 16:20:24 -0600

    Screen

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame jFrame = new JFrame("Pac Man");
+        jFrame.setVisible(true);
+        jFrame.setSize(boardWidth, boardHeight);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
